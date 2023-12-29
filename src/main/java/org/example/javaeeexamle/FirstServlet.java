3252d9db5548262535e2cd03f09a67c2a62a2b54
Konstantin-Pavlov <konstpavlov87@gmail.com>
2023-12-30 01:22:08 +0600

    add link to the home page

--- src/main/java/org/example/javaeeexamle/FirstServlet.java
+++ src/main/java/org/example/javaeeexamle/FirstServlet.java
@@ -31,8 +31,7 @@ public class FirstServlet extends HttpServlet {
         out.printf("<h1>%s</h1>%n", message);
         out.println("<h2>" + "wow" + " </h2>");
         out.println("</body></html>");
+        out.println("<br>  <a href=\"http://localhost:8080/\">homepage</a> <br>\n");
     }
 
-    public void destroy() {
-    }
 }
