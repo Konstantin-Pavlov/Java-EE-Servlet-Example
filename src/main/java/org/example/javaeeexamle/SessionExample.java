e497c9decada3820e9d75c834288f48cae9df89f
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-01-21 19:47:48 +0600

    add example with user authorization add redirection to show cart jsp page

--- src/main/java/org/example/javaeeexamle/SessionExample.java
+++ src/main/java/org/example/javaeeexamle/SessionExample.java
@@ -13,20 +13,54 @@ import java.io.PrintWriter;
 public class SessionExample extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        /*
+        // example with user authorization
+        String user = session.getAttribute("current_user");
+        if (user == null) {
+            // response for anonymous user
+            // authorization
+            // registration
+            session.setAttribute("current_user", id);
+        } else {
+            // response for authorized user
+        }*/
+
+
         PrintWriter out = resp.getWriter();
         HttpSession session = req.getSession();
+        String name = req.getParameter("name");
+        String surname = req.getParameter("surname");
         Integer sessionCounter = (Integer) session.getAttribute("sessionCounter");
+
+        Cart cart = (Cart) session.getAttribute("cart");
+        String productName = req.getParameter("pName");
+        int quantity = Integer.parseInt(req.getParameter("quantity"));
+
         if (sessionCounter == null) {
             sessionCounter = 1;
             session.setAttribute("sessionCounter", 1);
         }
         session.setAttribute("sessionCounter", sessionCounter + 1);
 
+        if (cart == null) {
+            cart = new Cart();
+            cart.setProductName(productName);
+            cart.setQuantity(quantity);
+        }
+        session.setAttribute("cart", cart);
+        session.setAttribute("name", name);
+        session.setAttribute("surname", surname);
+
+
         out.println("<html>");
         out.println("<h1>Session example page</h1>");
         out.printf("<p>you visited this page <b>%d</b> times</p>", sessionCounter);
         out.println("<br>");
-        out.println("<br>  <a href=\"http://localhost:8080/\">homepage</a> <br>\n");
+        out.println("<br>  <a href=\"http://localhost:8080/showCart.jsp\">show cart content</a> <br>\n");
+        out.println("<br>  <a href=\"http://localhost:8080/\"\">homepage</a> <br>\n");
         out.println("</html>");
+
+        // instant redirect to /showCart.jsp page
+//        getServletContext().getRequestDispatcher("/showCart.jsp").forward(req, resp);
     }
 }
